e1495d6c7dfe33df7e3e3b97b9ee6bb41139ec6d
ZERDICORP <56264511+ZERDICORP@users.noreply.github.com>
2022-04-02 23:48:10 +0300

    added echo_off & special characters escaping

--- src/win_build.bat
+++ src/win_build.bat
@@ -1,7 +1,10 @@
+@echo off
+
 javac -cp . com\adv_client\www\Main.java
   
 md ..\build
   
 jar -cfe ..\build\adv__client.jar com.adv__client.www.Main .
-  
-echo java -cp adv__client.jar com.adv_client.www.Main %* > ..\build\win_run.bat
+
+echo @echo off > ..\build\win_run.bat
+echo java -cp adv__client.jar com.adv_client.www.Main %%* >> ..\build\win_run.bat
